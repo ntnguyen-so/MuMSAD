cb6c2ab538d8bd853de38c9f7de944eaf8244a94
Ngoc Thanh Nguyen <thanh@krnmpi0.hvl.no>
2024-04-13 22:44:07 +0200

    prepare env for training

--- reproducibility_guide/train_deep_models.sh
+++ reproducibility_guide/train_deep_models.sh
@@ -1,74 +1,74 @@
 #!/bin/bash
 
 # Training Convnet
-python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
 python3 train_deep_model.py --path=data/OBSEA_512/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
 python3 train_deep_model.py --path=data/OBSEA_768/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=convnet --params=models/configuration/convnet_default.json --batch=256 --epochs=10 --eval-true
 
 # Training Inception Time
-python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_512/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_768/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=inception_time --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_512/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_768/ --split=0.7 --model=inception_time --params=models/configuration/inception_time_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=inception_time --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
 
 # Training Resnet
-python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
 python3 train_deep_model.py --path=data/OBSEA_512/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
 python3 train_deep_model.py --path=data/OBSEA_768/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=resnet --params=models/configuration/resnet_default.json --batch=256 --epochs=10 --eval-true
 
 # Training SiT with Convolutional Patch
-python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
 python3 train_deep_model.py --path=data/OBSEA_512/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
 python3 train_deep_model.py --path=data/OBSEA_768/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=sit --params=models/configuration/sit_conv_patch.json --batch=256 --epochs=10 --eval-true
 
 
 # Training SiT with Linear Patch
-python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
 python3 train_deep_model.py --path=data/OBSEA_512/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
 python3 train_deep_model.py --path=data/OBSEA_768/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=sit --params=models/configuration/sit_linear_patch.json --batch=256 --epochs=10 --eval-true
 
 
 # Training SiT with Original Stem
-python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
 python3 train_deep_model.py --path=data/OBSEA_512/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
 python3 train_deep_model.py --path=data/OBSEA_768/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=sit --params=models/configuration/sit_stem_original.json --batch=256 --epochs=10 --eval-true
 
 
 # Training SiT with ReLU Stem
-python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_16/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_32/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_64/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_128/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_256/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
 python3 train_deep_model.py --path=data/OBSEA_512/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
 python3 train_deep_model.py --path=data/OBSEA_768/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
-python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
+#python3 train_deep_model.py --path=data/OBSEA_1024/ --split=0.7 --model=sit --params=models/configuration/sit_stem_relu.json --batch=256 --epochs=10 --eval-true
